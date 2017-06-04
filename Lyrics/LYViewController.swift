05497930c9deb0ebabb6dfc5b5829891c85b7497
Vikas Shah <vik.k.shah@gmail.com>
2017-06-03 20:19:43 -0500

    Added project.

---
+++ Lyrics/LYViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LYViewController.swift
+//  Lyrics
+//
+//  Created by Vikas Shah on 6/1/17.
+//  Copyright © 2017 Vikas Shah. All rights reserved.
+//
+
+import UIKit
+
+class LYViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+}
